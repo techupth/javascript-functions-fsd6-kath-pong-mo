a44db79d24caccf94d71f15350a389b384c77ad7
Chavanvit <chavanvit.n@gmail.com>
2024-05-14 13:44:45 +0700

    Fead: Add ex java_function 1-6

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,14 @@
 // Start coding here
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => a * b;
+
+const divided = (a, b) => {
+  return a / b;
+};
+
+console.log();
